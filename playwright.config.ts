983e7e68f6b8a853e2160ab9bea4b53e3080bcdc
Parashurama <iamparashurama@gmail.com>
2026-06-24 16:53:46 +0530

    CICD commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   
     reporter: [
@@ -33,7 +33,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot : 'only-on-failure',
     video:'retain-on-first-failure',
-    headless : false,
+    headless : !process.env.CI ? false :true
   },
 
   /* Configure projects for major browsers */
