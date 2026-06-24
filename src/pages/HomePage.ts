805b1b7b399f7ce88950eaf6fafe5b8ad3a66469
Parashurama <iamparashurama@gmail.com>
2026-06-24 15:57:19 +0530

    Second commit changes

--- src/pages/HomePage.ts
+++ src/pages/HomePage.ts
@@ -9,7 +9,6 @@ export class HomePage extends BasePage{
     private readonly allHeaders : Locator;
     private readonly searchField : Locator;
     private readonly searchIcon : Locator;
-    private readonly user1 : Locator;
 
 
     //locators initilization through customer
@@ -20,7 +19,6 @@ export class HomePage extends BasePage{
         this.allHeaders = page.getByRole('heading', { level: 2 });
         this.searchField = page.getByRole('textbox', { name: 'Search' });
         this.searchIcon = page.locator('.btn.btn-default.btn-lg');
-        this.user1 = page.getByRole('link', { name: 'user1' });
     }
 
     //Public behaviours/actions to be defined
